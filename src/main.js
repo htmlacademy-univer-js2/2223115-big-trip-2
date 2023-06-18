a2ff778592b49a6af62f4c23c6d3cbdbcdd15120
Арина Андреевна <Arina.Sheshukova@urfu.me>
2023-06-18 23:17:27 +0500

    Added a model for filters

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ import { render } from './framework/render';
 import FiltersView from './view/filters';
 import TripPresenter from './presenter/trip-presenter';
 import PointsModel from './model/points-model';
+import FiltersModel from './model/filters-model';
 import generateFilter from './fish-data/filter';
 
 const filtersContainerElement = document.querySelector('.trip-controls__filters');
 const tripContainerElement = document.querySelector('.trip-events');
+
 const pointsModel = new PointsModel();
+const filtersModel = new FiltersModel();
 const tripPresenter = new TripPresenter(tripContainerElement, pointsModel);
 
 const filters = generateFilter(pointsModel.points)
 
 render(new FiltersView(filters), filtersContainerElement);
+
 tripPresenter.init();
---
+++ src/model/filters-model.js
@@ -0,0 +1,20 @@
+import Observable from "../framework/observable";
+import { FILTERS_TYPE, UpdateType } from "../const";
+
+class FiltersModel extends Observable {
+    constructor() {
+        super();
+        this._filter = FILTERS_TYPE.EVERYTHING;
+    }
+
+    get filter() {
+        return this._filter;
+    }
+
+    setFilter = (UpdateType, filter) => {
+        this._filter = filter;
+        this._notify(UpdateType, filter)
+    }
+}
+
+export default FiltersModel;
