275a1e77276add9a2ed633e53c52970710d08f0c
Арина Андреевна <Arina.Sheshukova@urfu.me>
2023-06-19 15:41:34 +0500

    Implemented the Adapter pattern (not working yet)

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -13,7 +13,7 @@ class PointsModel extends Observable {
     this._destinations = destinations;
 
     this._pointsApiService.points.then((points) => {
-      console.log(points);
+      console.log(points.map(this._adaptToClient));
     });
   }
 
@@ -34,6 +34,22 @@ class PointsModel extends Observable {
     }
     return this._destinations;
   }
+
+  _adaptToClient = (point) => {
+    const adaptedPoint = {...point,
+      basePrice: point['base_price'],
+      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      isFavorite: point['is_favorite']
+    };
+
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
+  };
 
   updatePoint = (updateType, update) => {
     const updatedIndex = this._points.findIndex((point) => point.id === update.id);
